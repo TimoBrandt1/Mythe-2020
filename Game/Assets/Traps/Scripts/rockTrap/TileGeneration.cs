4739d0acbda89556d9c8414e5153e2153b0391dc
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2020-05-12 15:28:26 +0200

    Started with tiles generation

--- Game/Assets/Traps/Scripts/rockTrap/TileGeneration.cs
+++ Game/Assets/Traps/Scripts/rockTrap/TileGeneration.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class TileGeneration : MonoBehaviour
 {
+    [SerializeField] private GameObject[] prefabTiles;
+    enum Tiles
+    {
+        COW,
+        PIG,
+        SHEEP,
+        DEATH
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        int[,] intMap;
+        intMap = new int[,]{
+            {0, 0, 0, 0},
+            {1, 1, 1, 1 }
+        };
+        for (int i = 0; i < intMap.Length; i++)
+        {
+            for (int j = 0; j < intMap.Length; j++)
+            {
+                GameObject aTile = Instantiate(prefabTiles[intMap[i, j]]);
+                aTile.transform.position = new Vector3(
+                    (aTile.GetComponent<Renderer>().bounds.size.x / 2) * i,
+                    0,
+                    (aTile.GetComponent<Renderer>().bounds.size.z / 2) * j
+                    );
+            }
+        }
     }
 
     // Update is called once per frame
