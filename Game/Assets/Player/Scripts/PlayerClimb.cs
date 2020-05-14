c5e139ea38fe4d9b254aba06c4386d2d7cb2355b
Royd54 <duponroy@gmail.com>
2020-05-14 17:51:53 +0200

    Improved the climbing system
    Made the system dynamic. The player also climbs up on the ledge at dynamic spots (closest point on the object)

--- Game/Assets/Player/Scripts/PlayerClimb.cs
+++ Game/Assets/Player/Scripts/PlayerClimb.cs
@@ -6,14 +6,14 @@ using System;
 public class PlayerClimb : MonoBehaviour
 {
     public GameObject ledge1;
-    public GameObject ledge2;
 
     public delegate void OnPlayerClimb();
     public delegate void OnPlayerLoose();
     public event Action onPlayerClimb;
     public event Action onPlayerLoose;
+    private float dist;
+    private bool grabStart = true;
 
-    public Transform temp;
 
     // Start is called before the first frame update
     void Start()
@@ -24,12 +24,14 @@ public class PlayerClimb : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float dist = Vector3.Distance(ledge1.transform.position, transform.position);
+        FindClosestEnemy();
+        dist = Vector3.Distance(ledge1.transform.position, transform.position);
         if (dist < 5 && Input.GetKey(KeyCode.Space))
         {
+                setFirstPos();
                 transform.rotation = ledge1.transform.rotation;
                 transform.position = new Vector3(Mathf.Clamp(transform.position.x, ledge1.transform.position.x - ledge1.transform.localScale.x/2, ledge1.transform.position.x + ledge1.transform.localScale.x/2),
-                ledge1.transform.position.y,
+                ledge1.transform.position.y - 1f,
                 Mathf.Clamp(transform.position.z, ledge1.transform.position.z - ledge1.transform.localScale.x / 2, ledge1.transform.position.z + ledge1.transform.localScale.x / 2));
 
                 if (Input.GetKey(KeyCode.D))
@@ -52,6 +54,38 @@ public class PlayerClimb : MonoBehaviour
             GetComponent<playerMove>().enabled = true;
             GetComponent<ThirdPersonCharacterControl>().enabled = true;
             onPlayerLoose();
+            grabStart = true;
+        }
+    }
+
+    void FindClosestEnemy()
+    {
+        float distanceToClosestEnemy = Mathf.Infinity;
+        Ledge closestEnemy = null;
+        Ledge[] allEnemies = GameObject.FindObjectsOfType<Ledge>();
+
+        foreach (Ledge currentEnemy in allEnemies)
+        {
+            float distanceToEnemy = (currentEnemy.transform.position - this.transform.position).sqrMagnitude;
+            if (distanceToEnemy < distanceToClosestEnemy)
+            {
+                distanceToClosestEnemy = distanceToEnemy;
+                closestEnemy = currentEnemy;
+                ledge1 = currentEnemy.gameObject;
+            }
+        }
+
+        Debug.DrawLine(this.transform.position, closestEnemy.transform.position);
+    }
+
+    public void setFirstPos()
+    {
+        if (grabStart)
+        {
+            Vector3 closestPoint = ledge1.GetComponent<BoxCollider>().ClosestPointOnBounds(this.transform.position);
+            transform.position = closestPoint;
+           // transform.position = new Vector3(ledge1.transform.position.x, ledge1.transform.position.y, ledge1.transform.position.z);
+            grabStart = false;
         }
     }
 }
