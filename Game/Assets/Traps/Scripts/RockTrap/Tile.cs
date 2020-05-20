5dbbbde1cdbcd7c9ad4ae429680485b5d743f964
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2020-05-20 14:38:16 +0200

    Rock Shooting trap almost done

--- Game/Assets/Traps/Scripts/RockTrap/Tile.cs
+++ Game/Assets/Traps/Scripts/RockTrap/Tile.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             //Scream Tile Type to TileChecker
+            Debug.Log(gameObject.tag);
+            GameObject.Find("Tiles").GetComponent<TileChecker>().walkPatern.Add(gameObject.tag);
         }
     }
 }
--- Game/Assets/Traps/Scripts/rockTrap/TileChecker.cs
+++ Game/Assets/Traps/Scripts/rockTrap/TileChecker.cs
@@ -5,21 +5,44 @@ using UnityEngine;
 public class TileChecker : MonoBehaviour
 {
     //Get first 3 tiles that have been walked on and make it the partern
-    private int[] startPatern;
-    private int[] walkPatern;
+    private List<string> startPatern = new List<string>();
+    public List<string> walkPatern = new List<string>();
 
     private void Start()
     {
-        
+        startPatern.Add("Cow");
+        startPatern.Add("Pig");
+        startPatern.Add("Sheep");
+        startPatern.Clear();
     }
 
     private void Update()
     {
-        
+        CheckWalkPaternSize();
     }
 
-    private void ComparePatern()
+    private void ComparePaterns()
     {
-        //compare the start patern with the walk patern
+        if (true)
+        {
+
+        }
+        else
+        {
+
+        }
+            GameObject.Find("SpawnPoints").GetComponent<RockSpawning>().TrapTriggerd();
+    }
+    private void CheckWalkPaternSize()
+    {
+        if (walkPatern.Count == 3)
+        {
+            ComparePaterns();
+            walkPatern.Clear();
+        }
+        if (walkPatern.Count > 3)
+        {
+            walkPatern.Clear();
+        }
     }
 }
