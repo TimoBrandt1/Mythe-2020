ae925177c11ffbfd55870dcf469dd6fe0fbf5154
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2020-05-25 09:54:51 +0200

    Tiles Fully Work
    If you now wolk the wrong tile patern then you are dead

--- Game/Assets/Traps/Scripts/rockTrap/TileChecker.cs
+++ Game/Assets/Traps/Scripts/rockTrap/TileChecker.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class TileChecker : MonoBehaviour
 {
-    //Get first 3 tiles that have been walked on and make it the partern
     private List<string> startPatern = new List<string>();
     public List<string> walkPatern = new List<string>();
+
+    
 
     private void Start()
     {
         startPatern.Add("Cow");
         startPatern.Add("Pig");
         startPatern.Add("Sheep");
-        startPatern.Clear();
     }
 
     private void Update()
@@ -21,28 +21,43 @@ public class TileChecker : MonoBehaviour
         CheckWalkPaternSize();
     }
 
-    private void ComparePaterns()
+    private void CheckLists()
     {
-        if (true)
-        {
 
+        if (ListEquals(startPatern, walkPatern))
+        {
+            Debug.Log("List 1 is the same as list 2");
+            
         }
         else
         {
-
-        }
+            Debug.Log("List 1 does not equal list 2");
             GameObject.Find("SpawnPoints").GetComponent<RockSpawning>().TrapTriggerd();
+        }
     }
+
     private void CheckWalkPaternSize()
     {
         if (walkPatern.Count == 3)
         {
-            ComparePaterns();
+            CheckLists();
             walkPatern.Clear();
         }
         if (walkPatern.Count > 3)
         {
             walkPatern.Clear();
         }
+    }
+
+    public static bool ListEquals<T>(IList<T> list1, IList<T> list2)
+    {
+        for (int i = 0; i < list1.Count; i++)
+        {
+            if (!list1[i].Equals(list2[i]))
+            {
+                return false;
+            }
+        }     
+        return true;
     }
 }
