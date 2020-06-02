242d72bcafdeb72117c3c2010cc68a9f2d36fbcf
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2020-06-02 13:12:16 +0200

    Started with Pendulum

---
+++ Game/Assets/Traps/Scripts/Pendulum.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float leftPushRange;
+    [SerializeField] private float rightPushRange;
+    [SerializeField] private Vector3 velocityThreshold;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity.z = velocityThreshold.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Push();
+    }
+    private void Push()
+    {
+        if (transform.rotation.z > 0
+            && transform.rotation.z < rightPushRange
+            && rb.angularVelocity > velocityThreshold)
+        {
+            rb.angularVelocity = velocityThreshold;
+        }
+        else if (true)
+        {
+            rb.angularVelocity = velocityThreshold * -1;
+        }
+    }
+}
