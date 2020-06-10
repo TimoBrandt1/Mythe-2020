36ad4a61c9fbd566a77961d0ce5dcc4517ab9903
Royd54 <duponroy@gmail.com>
2020-06-10 21:23:08 +0200

    Added an simple inventory system
    Added an inventory system. This system relies on scriptable objects. Adds objects to the backpack when in distance / input is given. Also added LevelComplete script, so that the player can complete the level. This script checks if the player found every item..

---
+++ Game/Assets/Player/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public Item[] itemArray = new Item[3];
+    public bool full = false;
+
+    public bool AddItem(Item item)
+    {
+        for(int i = 1; i < itemArray.Length; i++)
+        {
+            Debug.Log(itemArray[i]);
+            if (itemArray[i] == null)
+            {
+                itemArray[i] = item;
+                full = false;
+                return true;
+            }
+        }
+        full = true;
+        return false;
+    }
+}
