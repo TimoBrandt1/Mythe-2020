bd5bf214ec541fae9972968adbb5c84b5af6adf0
jvdlem <jordivanderlem@gmail.com>
2020-06-16 14:10:05 +0200

    Fixed textsystem

---
+++ Game/Assets/UI/TextHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextHandler : MonoBehaviour
+{
+    [SerializeField] private AudioSource talkingSound;
+    [SerializeField] private GameObject textImage;
+    private Text messageText;
+    private string currentText;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        messageText = transform.Find("message").Find("Text").GetComponent<Text>();
+    }
+    private void Update()
+    {
+    }
+
+    public void StartWriter(string textToWrite, float timePerCharacter, float cleanText)
+    {
+        StartCoroutine(WriteText(textToWrite, timePerCharacter,cleanText));
+    }
+    private IEnumerator WriteText(string textToWrite, float timePerCharacter, float cleanText)
+    {
+        talkingSound.Play();
+        for (int i = 0; i < textToWrite.Length+1; i++)
+        {
+            currentText = textToWrite.Substring(0, i);
+            messageText.text = currentText;
+            yield return new WaitForSeconds(timePerCharacter);
+        }
+        talkingSound.Stop();
+
+    }
+}
--- Game/Assets/UI/TextTrigger.cs
+++ Game/Assets/UI/TextTrigger.cs
@@ -5,20 +5,37 @@ using UnityEngine.UI;
 
 public class TextTrigger : PressurPlate
 {
+
     [SerializeField] private bool canSetText;
-    [SerializeField] private UI_Assistant ui_Assistant;
-    [SerializeField] private Text textToSay;
+    [SerializeField] private TextHandler textHandler;
+    [SerializeField] private string[] texts;
+    [SerializeField] private float[] timePerCharacter;
+    [SerializeField] private float cleanTime;
+    private bool nextText;
     private void Start()
     {
         canSetText = true;
-        ui_Assistant = GameObject.Find("UI_Assistant").GetComponent<UI_Assistant>();
+        textHandler = GameObject.Find("TextHandeler").GetComponent<TextHandler>();
     }
     private void OnTriggerEnter(Collider other)
     {
         if (canSetText)
         {
-            ui_Assistant.Awake();
+            
+            StartCoroutine(PassText());
+            Debug.Log("geef text door");
         }
     }
     private void OnTriggerExit(Collider other){ canSetText = false;}
+    private IEnumerator PassText()
+    {
+
+        for (int i = 0; i < texts.Length;)
+            {
+                textHandler.StartWriter(texts[i], timePerCharacter[i], cleanTime);
+                Debug.Log("gaat schrijven");
+                yield return new WaitForSeconds((timePerCharacter[i]*texts[i].Length)+1);          
+                i++;  
+            }
+        }
 }
